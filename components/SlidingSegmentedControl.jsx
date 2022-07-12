3b28b08712c2847dd5e4592e0116f0256baf8ee0
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2022-07-12 05:29:45 -0300

    chakra SlidingSegmentedControl to tailwind

--- components/SlidingSegmentedControl.jsx
+++ components/SlidingSegmentedControl.jsx
@@ -1,11 +1,11 @@
-import { Box, Flex } from '@chakra-ui/react';
 import { useState } from 'react';
 
 export default function SlidingSegmentedControl({ options, selectedValue, setSelectedValue }) {
   const [selectedIndex, setSelectedIndex] = useState(
     options.map((option) => option.value).indexOf(selectedValue),
   );
-  const optionWidth = `${100 / options.length}%`;
+  const optionWidth = 100 / options.length;
+  const optionLeft = selectedIndex * optionWidth;
 
   const onClick = (index) => {
     setSelectedIndex(index);
@@ -13,55 +13,32 @@ export default function SlidingSegmentedControl({ options, selectedValue, setSel
   };
   return (
     // Frame
-    <Flex justifyContent="space-around" alignItems="center" w="full" borderRadius={6} position="relative" bg="blackAlpha.300">
-      {options.map(({ value }, index) => index !== options.length - 1 && (
+    <div className="flex justify-around items-center w-full rounded-md relative bg-black/20">
+      <div className="absolute w-full top-0 bottom-0 right-0 left-0 flex justify-evenly items-center">
+        {options.map(({ value }, index) => index !== options.length - 1 && (
         // Divider
-        <Box
-          position="absolute"
-          key={`divider-${value}`}
-          left={`calc( ${optionWidth} * ${index + 1} )`}
-          h="55%"
-          w="1px"
-          bg="blackAlpha.300"
-        />
-      ))}
+        <div key={`divider-${value}`} className="h-[55%] w-[1px] bg-black/20" />
+        ))}
+      </div>
       {/* Background Frame */}
-      <Box
-        transition=".4s"
-        left={`calc( ${selectedIndex} * ${optionWidth} )`}
-        top={0}
-        position="absolute"
-        w={`calc( ${optionWidth} )`}
-        h="100%"
-        p="1"
+      <div
+        style={{ left: `${optionLeft}%`, width: `${optionWidth}%` }}
+        className="p-1 h-full absolute top-0 duration-300"
       >
         {/* Background */}
-        <Box
-          borderRadius="md"
-          h="100%"
-          borderWidth="1px"
-          boxShadow="lg"
-          bg="white"
-        />
-      </Box>
+        <div className="rounded-md h-full border shadow-md bg-white" />
+      </div>
       {options.map(({ value, label }, index) => (
         // Option
-        <Flex
-          color="blackAlpha.900"
-          fontSize="xl"
-          w="100%"
-          alignItems="center"
-          justifyContent="center"
+        <button
+          type="button"
           key={`option-${value}`}
-          position="relative"
           onClick={() => onClick(index)}
-          cursor="pointer"
-          px="2"
-          py="1"
+          className="py-1 px-2 cursor-pointer relative flex justify-center items-center w-full text-lg text-gray-900"
         >
           {label}
-        </Flex>
+        </button>
       ))}
-    </Flex>
+    </div>
   );
 }
